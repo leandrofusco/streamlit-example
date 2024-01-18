f1064c5ca598ceb0cc7dfe148579f6a552e46494
Leandro Fusco <61833359+leandrofusco@users.noreply.github.com>
2024-01-18 20:11:04 -0300

    Update streamlit_app.py
    Update coding

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,26 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+path = 'modelo_falhas.pkl'
+model = joblib.load(path)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Função para fazer previsões
+def fazer_previsao(features):
+    # Adapte conforme necessário para o seu modelo
+    previsao = model.predict(features.reshape(1, -1))
+    return previsao[0]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+feature_input = st.text_input('Insira os valores das características (separados por vírgula):')
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if st.button('Prever'):
+    # Converter entrada em uma lista de float
+    features = [float(valor) for valor in feature_input.split(',')]
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    # Fazer previsão
+    resultado = fazer_previsao(np.array(features))
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    # Exibir resultado
+    if resultado == 1:
+        st.write('Resultado da Detecção de Falhas: Falha detectada!')
+    else:
+        st.write('Resultado da Detecção de Falhas: Sem falha detectada.')
